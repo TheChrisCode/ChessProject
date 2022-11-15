35780004c59212b6985f5b16c3325664d44e6dce
TheChrisCode <christianphillips.thecoder@gmail.com>
2022-11-15 11:07:14 -0600

    This is another update.

--- src/main/java/com/example/chessproject/ChessBoard.java
+++ src/main/java/com/example/chessproject/ChessBoard.java
@@ -1,13 +1,20 @@
 package com.example.chessproject;
+import javafx.event.EventHandler;
+import javafx.scene.control.Cell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Scene;
 
+import java.io.FileInputStream;
+import java.nio.file.attribute.GroupPrincipal;
 import java.util.Arrays;
 
 
@@ -17,20 +24,24 @@ public class ChessBoard extends Application {
     public void start(Stage stage) throws Exception {
         int a = 0;
         int b = 1;
-        Image image = new Image("black_pawn.jpg");
+        StackPane pawn = new StackPane();
+        Image image = new Image(new FileInputStream("C:\\Users\\chris\\IdeaProjects\\ChessProject\\src\\main\\resources\\com\\example\\chessproject\\black_pawn.jpg"));
+
         ImageView iv1 = new ImageView();
-        iv1.setImage(image);
         iv1.setFitHeight(64);
         iv1.setFitWidth(64);
+        iv1.setPreserveRatio(false);
+        iv1.setImage(image);
+
         Rectangle [] rectangles = new Rectangle[64];
         for (int i = 0; i < 64; i++) {
             rectangles[i] = new Rectangle(0, 0, 64, 64);
 
         }
-
+        pawn.getChildren().addAll(rectangles[0], iv1);
         System.out.println(Arrays.toString(rectangles));
 
-        GridPane gridPane=new GridPane();
+        GridPane gridPane= new GridPane();
         Scene scene = new Scene(gridPane,512,512);
 
         for (int i = 0; i < 64; i++) {
@@ -51,6 +62,9 @@ public class ChessBoard extends Application {
             System.out.println(a);
         }
 
+
+        GridPane.setConstraints(pawn, 4, 3);
+        gridPane.getChildren().add(pawn);
 
         stage.setScene(scene);
         stage.show();
